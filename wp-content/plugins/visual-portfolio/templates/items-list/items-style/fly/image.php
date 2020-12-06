2ace214be3535456e38e40cf08d442ed60892811
rgulati <rgulati@avaloninfosys.com>
2020-12-06 18:19:22 +0530

    plugins

---
+++ wp-content/plugins/visual-portfolio/templates/items-list/items-style/fly/image.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Item image template.
+ *
+ * @var $args
+ * @var $opts
+ *
+ * @package visual-portfolio
+ */
+
+// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+$link_data = array(
+    'href'   => $args['url'],
+    'target' => $args['url_target'],
+    'rel'    => $args['url_rel'],
+);
+
+?>
+
+<div class="vp-portfolio__item-img-wrap">
+    <div class="vp-portfolio__item-img">
+        <?php visual_portfolio()->include_template( 'global/link-start', $link_data ); ?>
+
+        <?php echo wp_kses( $args['image'], $args['image_allowed_html'] ); ?>
+
+        <?php visual_portfolio()->include_template( 'global/link-end', $link_data ); ?>
+    </div>
+</div>
---
+++ wp-content/plugins/visual-portfolio/templates/items-list/items-style/image.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Item image template.
+ *
+ * @var $args
+ * @var $opts
+ *
+ * @package visual-portfolio
+ */
+
+// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+$link_data = array(
+    'href'   => $args['url'],
+    'target' => $args['url_target'],
+    'rel'    => $args['url_rel'],
+);
+
+?>
+
+<div class="vp-portfolio__item-img-wrap">
+    <div class="vp-portfolio__item-img">
+        <?php visual_portfolio()->include_template( 'global/link-start', $link_data ); ?>
+
+        <?php echo wp_kses( $args['image'], $args['image_allowed_html'] ); ?>
+
+        <div class="vp-portfolio__item-img-overlay">
+            <?php
+            // Show Icon.
+            visual_portfolio()->include_template(
+                'items-list/item-parts/icon',
+                array(
+                    'args' => $args,
+                    'opts' => $opts,
+                )
+            );
+            ?>
+        </div>
+
+        <?php visual_portfolio()->include_template( 'global/link-end', $link_data ); ?>
+    </div>
+</div>
