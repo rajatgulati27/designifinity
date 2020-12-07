01ea0f8c878d0e95ff473524563e1ced92cc505a
rgulati <rgulati@avaloninfosys.com>
2020-12-07 15:31:25 +0530

    content.php

--- wp-content/themes/popularfx/template-parts/content.php
+++ wp-content/themes/popularfx/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -9,17 +10,17 @@
 
 ?>
 
-<article class="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>	
+<article class="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+		if (is_singular()) :
+			the_title('<h1 class="entry-title">', '</h1>');
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
 		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
+		if ('post' === get_post_type()) :
+		?>
 			<div class="entry-meta">
 				<?php
 				popularfx_posted_on();
@@ -33,27 +34,27 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'popularfx' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'popularfx' ),
-				'after'  => '</div>',
-			)
-		);
+		//the_content(
+		//	sprintf(
+		//		wp_kses(
+		//			/* translators: %s: Name of current post. Only visible to screen readers */
+		//			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'popularfx' ),
+		//			array(
+		//				'span' => array(
+		//					'class' => array(),
+		//				),
+		//			)
+		//		),
+		//		wp_kses_post( get_the_title() )
+		//	)
+		//);
+		//
+		//wp_link_pages(
+		//	array(
+		//		'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'popularfx' ),
+		//		'after'  => '</div>',
+		//	)
+		//);
 		?>
 	</div><!-- .entry-content -->
 
